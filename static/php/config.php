eea42f8ec45f9e781015e9b62d464df290cd812b
eden reinfurt <eden@edenwilliamreinfurt.com>
2023-08-24 14:16:45 +0000

    change images to color

--- static/php/config.php
+++ static/php/config.php
@@ -3,12 +3,12 @@
     site-specific config
 */
 
-$site_title = 'w-w-w';
-$head = 'Hello';
-$site = 'w-w-w-orld.';
+$site_title = 'edenwilliamreinfurt';
+$head = 'eden';
+$site = '!';
 $home = $head . ", " . $site;
 $card_default = '/media/jpg/card-default.jpg';
-$description = 'Hello, w-w-w-orld.';
+$description = 'eden!';
 $site_url = isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] != 'off' ? 'https' : 'http';
 $site_url .= '://' . $_SERVER['SERVER_NAME'];
 ?>
